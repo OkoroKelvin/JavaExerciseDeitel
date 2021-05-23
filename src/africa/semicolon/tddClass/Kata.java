64497d97c8b935fcf6af8b188d73e95bfc957960
Kelvin Okoro <okorokelvinemoakpooke@yahoo.com>
2021-05-23 18:29:55 +0100

    My Kata Class Application

--- src/africa/semicolon/tddClass/Kata.java
+++ src/africa/semicolon/tddClass/Kata.java
@@ -2,7 +2,13 @@ package africa.semicolon.tddClass;
 import java.util.Scanner;
 
 public class Kata {
-    private String grade;
+    public String grade;
+    public int totalPrice;
+    public int costPrice;
+    public int sellerPrice;
+    public int resellerProfit;
+
+
     public static void main(String... args) {
         int num1;
         System.out.println("kindly input three numbers");
@@ -20,33 +26,84 @@ public class Kata {
         }
     }
 
-    public String showGrade(int score){
-        if (score>=90){
-            grade="A";
+    public String showGrade(int score) {
+        if (score >= 90) {
+            grade = "A";
+        } else if (score >= 80 && score <= 89) {
+            grade = "B";
+        } else if (score >= 70 && score <= 79) {
+            grade = "C";
+        } else if (score >= 60 && score <= 69) {
+            grade = "D";
+        } else if (score >= 40 && score <= 59) {
+            grade = "E";
+        } else if (score <= 39) {
+            grade = "F";
         }
-        else
-            if(score>=80 && score<=89){
-                grade="B";
-            }
-        else
-            if(score>=70 && score<=79){
-                grade="C";
-            }
-        else
-            if(score>=60 && score<=69){
-                grade="D";
-            }
-        else
-            if(score>=40 && score<=59){
-                grade="E";
-            }
-        else
-            if(score<=39){
-                grade="F";
-            }
         return grade;
     }
 
+    public int totalPriceTestDriller(int copies) {
+        if (copies >= 1 && copies <= 4) {
+            totalPrice = copies * 1500;
+        } else if (copies >= 5 && copies <= 9) {
+            totalPrice = copies * 1400;
+        } else if (copies >= 10 && copies <= 29) {
+            totalPrice = copies * 1200;
+        } else if (copies >= 30 && copies <= 49) {
+            totalPrice = copies * 1100;
+        } else if (copies >= 50 && copies <= 99) {
+            totalPrice = copies * 1000;
+        } else if (copies >= 100 && copies <= 199) {
+            totalPrice = copies * 900;
+        } else if (copies >= 200) {
+            totalPrice = copies * 800;
+        }
+        return totalPrice;
+    }
 
+    public int displayProfit(int copies) {
+        if (copies >= 1 && copies <= 4) {
+            costPrice = 1500;
+            sellerPrice = 2000;
+            resellerProfit = (sellerPrice * copies) - (costPrice * copies);
+        }else
+            if (copies >= 5 && copies <= 9) {
+                costPrice = 1400;
+                sellerPrice = 2000;
+                resellerProfit = (sellerPrice * copies) - (costPrice * copies);
+            }
+        else
+            if(copies >= 10 && copies <= 29){
+                costPrice=1200;
+                sellerPrice = 2000;
+                resellerProfit = (sellerPrice * copies) - (costPrice * copies);
+            }
+        else
+            if(copies >= 30 && copies <= 49){
+                costPrice=1100;
+                sellerPrice = 2000;
+                resellerProfit = (sellerPrice * copies) - (costPrice * copies);
+            }
+        else
+            if(copies>=50 && copies <=99){
+                costPrice=1000;
+                sellerPrice=2000;
+                resellerProfit = (sellerPrice * copies) - (costPrice * copies);
+            }
+        else
+            if (copies>=100 && copies<=199){
+                costPrice=900;
+                sellerPrice=2000;
+                resellerProfit = (sellerPrice * copies) - (costPrice * copies);
+            }
+        else
+            if (copies>=200){
+                costPrice=800;
+                sellerPrice=2000;
+                resellerProfit = (sellerPrice * copies) - (costPrice * copies);
+            }
+        return resellerProfit;
+    }
 
 }
--- test/africa/semicolon/tddClass/KataTest.java
+++ test/africa/semicolon/tddClass/KataTest.java
@@ -36,5 +36,44 @@ class KataTest {
         Kata myKata= new Kata();
         assertEquals("F",myKata.showGrade(20));
     }
+    @Test
+    public void testToKnowTotalPriceOfTestDrillerProductOneToFourCopies(){
+        Kata myKata= new Kata();
+        assertEquals(3000, myKata.totalPriceTestDriller(2));
+    }
+    @Test
+    public void testToKnowTotalPriceOfTestDrillerProductFiveToNineCopies(){
+        Kata myKata= new Kata();
+        myKata.totalPriceTestDriller(7);
+        assertEquals(9800,myKata.totalPriceTestDriller(7));
+    }
+    @Test
+    public void testToKnowTotalPriceOfTestDrillerProduct30To49Copies(){
+        Kata myKata= new Kata();
+        assertEquals(44000,myKata.totalPriceTestDriller(40));
+    }
+    @Test
+    public void testToKnowTotalPriceOfTestDrillerProduct50To99Copies(){
+        Kata myKata=new Kata();
+        assertEquals(50000,myKata.totalPriceTestDriller(50));
+    }
+    @Test
+    public void testToKnowTotalPriceOfTestDrillerProduct100To200Copies(){
+        Kata myKata=new Kata();
+        assertEquals(90000,myKata.totalPriceTestDriller(100));
+    }
+    @Test
+    public void testToKnowTotalPriceOfTestDrillerProductAbove200Copies(){
+        Kata myKata= new Kata();
+        assertEquals(160800,myKata.totalPriceTestDriller(201));
+    }
+    @Test
+    public void testToKnowTheProfitOfTheSeller(){
+        Kata myKata=new Kata();
+        assertEquals(1000,myKata.displayProfit(2));
+        assertEquals(1500,myKata.displayProfit(3));
+        assertEquals(3000,myKata.displayProfit(5));
+        assertEquals(36000,myKata.displayProfit(40));
 
+    }
 }
