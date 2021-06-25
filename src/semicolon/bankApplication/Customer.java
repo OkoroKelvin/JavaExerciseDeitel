686811134119543fc83cc5b36693b7ea02fa8613
Kelvin Okoro <okorokelvinemoakpooke@yahoo.com>
2021-06-25 09:37:30 +0100

    Update bank application

---
+++ src/semicolon/bankApplication/Customer.java
@@ -0,0 +1,11 @@
+//package semicolon.bankApplication;
+//
+//public class Customer {
+//
+//    String prompt = """
+//    Welcome to the Bank Phb
+//
+//   Press 1. To Open an Account
+//   Press 2.
+//"""
+//}
